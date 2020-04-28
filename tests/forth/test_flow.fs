f605e2d85c22ec6ac16b59230f132ee54f2400a7
Virgil Dupras <hsoft@hardcoded.net>
2020-04-27 20:44:21 -0400

    Give CASE's default branch access to its input value
    ref #97

--- tests/forth/test_flow.fs
+++ tests/forth/test_flow.fs
@@ -3,11 +3,12 @@
         'X' OF 42 ENDOF
         0x12 OF 43 ENDOF
         255 OF 44 ENDOF
-        45
+        1+
     ENDCASE
 ;
 
 'X' foo 42 #eq
 0x12 foo 43 #eq
 255 foo 44 #eq
-254 foo 45 #eq
+254 foo 255 #eq
+'S S0 #eq
