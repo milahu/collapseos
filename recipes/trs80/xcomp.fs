986a40d3e2328738e2583793d8188b8704a3f3f3
Virgil Dupras <hsoft@hardcoded.net>
2020-05-04 09:21:10 -0400

    trs80: override NL to avoid double newlines

--- recipes/trs80/xcomp.fs
+++ recipes/trs80/xcomp.fs
@@ -20,5 +20,6 @@ PC ORG @ 8 + !
 ," CURRENT @ HERE ! "
 422 459 XPACKR ( core print readln fmt blk )
 499 500 XPACKR ( trs80.fs )
-," : _ BLK$ FD$ (ok) RDLN$ ; _ "
+( 0x0a == NLPTR. TRS-80 wants CR-only newlines )
+," : _ ['] CR 0x0a RAM+ ! BLK$ FD$ (ok) RDLN$ ; _ "
 H@ 256 /MOD 2 PC! 2 PC!
