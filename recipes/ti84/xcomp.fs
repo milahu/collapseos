2f1e635b9d361507c9c0b31d61d5ca0e64dc3aae
Virgil Dupras <hsoft@hardcoded.net>
2020-05-09 08:50:55 -0400

    ti84: tidy up driver code
    Pushed all words directly interfacing with ports and memory offsets to
    low level layers. This saves us the need for keeping those variables in
    runtime memory.

--- recipes/ti84/xcomp.fs
+++ recipes/ti84/xcomp.fs
@@ -1,6 +1,6 @@
 0x8000 CONSTANT RAMSTART
 0xb000 CONSTANT RS_ADDR
-RAMSTART 0x70 + CONSTANT TI_MEM
+RAMSTART 0x70 + CONSTANT LCD_MEM
 212 LOAD  ( z80 assembler )
 262 LOAD  ( xcomp )
 522 LOAD  ( font compiler )
@@ -12,15 +12,15 @@ RAMSTART 0x70 + CONSTANT TI_MEM
 CURRENT @ XCURRENT !
 
 282 LOAD  ( boot.z80 )
-555 LOAD  ( ti.z80 )
 393 LOAD  ( icore low )
+555 557 LOADR ( ti low )
 415 LOAD  ( icore high )
 (entry) ~FNT CPFNT3x5
 (entry) _
 ( Update LATEST )
 PC ORG @ 8 + !
 422 437 XPACKR ( core )
-556 560 XPACKR ( ti )
+558 560 XPACKR ( ti high )
 438 446 XPACKR ( print fmt )
 ," : _ LCD$ LIT< Hello (print) LIT< World! (print) BYE ; _ "
 ORG @ 256 /MOD 2 PC! 2 PC!
