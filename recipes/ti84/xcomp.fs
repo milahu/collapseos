4ce0727c7266f0ff1fdabc119f0a31c747bba933
Virgil Dupras <hsoft@hardcoded.net>
2020-05-09 11:55:58 -0400

    ti84: kbd driver wip

--- recipes/ti84/xcomp.fs
+++ recipes/ti84/xcomp.fs
@@ -1,27 +1,30 @@
 0x8000 CONSTANT RAMSTART
 0xb000 CONSTANT RS_ADDR
 RAMSTART 0x70 + CONSTANT LCD_MEM
+0x01 CONSTANT KBD_PORT
 212 LOAD  ( z80 assembler )
 262 LOAD  ( xcomp )
 522 LOAD  ( font compiler )
 : CODE XCODE ;
 : IMMEDIATE XIMM ;
 : (entry) (xentry) ;
+: CREATE XCREATE ; ( for KBD tbls )
 : : [ ' X: , ] ;
 
 CURRENT @ XCURRENT !
 
 282 LOAD  ( boot.z80 )
 393 LOAD  ( icore low )
-555 557 LOADR ( ti low )
+555 557 LOADR ( LCD low )
+566 569 LOADR ( KBD low )
 415 LOAD  ( icore high )
 (entry) ~FNT CPFNT3x5
 (entry) _
 ( Update LATEST )
 PC ORG @ 8 + !
 422 437 XPACKR ( core )
-558 560 XPACKR ( ti high )
+558 560 XPACKR ( LCD high )
 438 446 XPACKR ( print fmt )
-," : _ LCD$ LIT< Hello (print) LIT< World! (print) BYE ; _ "
+," : _ LCD$ LIT< Hello (print) (key) (emit) BYE ; _ "
 ORG @ 256 /MOD 2 PC! 2 PC!
 H@ 256 /MOD 2 PC! 2 PC!
