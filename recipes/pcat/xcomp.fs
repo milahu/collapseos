8c452f5add50a361ec060a4ac65482e4fd66f463
Virgil Dupras <hsoft@hardcoded.net>
2020-06-14 21:52:58 -0400

    pcat: begin implementing find
    Not comparing strings yet. Without asm tooling support, it's a big
    chunk to swallow at once. It's progressing well!

--- recipes/pcat/xcomp.fs
+++ recipes/pcat/xcomp.fs
@@ -1,6 +1,9 @@
 750 LOAD  ( 8086 asm )
 262 LOAD  ( xcomp )
 270 LOAD  ( xcomp overrides )
-812 814 LOADR
+812 816 LOADR
+(entry) _
+( Update LATEST )
+PC ORG @ 8 + !
 ORG @ 256 /MOD 2 PC! 2 PC!
 H@ 256 /MOD 2 PC! 2 PC!
