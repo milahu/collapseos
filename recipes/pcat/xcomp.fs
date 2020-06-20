1536434666638262327af0548ce659b07e42fdfe
Virgil Dupras <hsoft@hardcoded.net>
2020-06-20 13:50:01 -0400

    pcat: implement cell execution and shaping things up

--- recipes/pcat/xcomp.fs
+++ recipes/pcat/xcomp.fs
@@ -4,7 +4,14 @@ RS_ADDR 0x80 - CONSTANT RAMSTART
 750 LOAD  ( 8086 asm )
 262 LOAD  ( xcomp )
 270 LOAD  ( xcomp overrides )
-812 828 LOADR
+812 827 LOADR
+353 LOAD  ( xcomp core low )
+CODE (emit)
+    AX POPx, AH 0x0e MOVri, ( print char ) 0x10 INT,
+;CODE
+CODE (key) AH AH XORrr, 0x16 INT, AX PUSHx, ;CODE
+: FOO (key) (emit) ;
+: BOOT 0x08 @ LIT< FOO _find DROP EXECUTE BYE ;
 (entry) _
 ( Update LATEST )
 PC ORG @ 8 + !
