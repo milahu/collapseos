293a561ab218fb1ca059d6bc3e94b90ec51dd88a
Virgil Dupras <hsoft@hardcoded.net>
2020-07-05 15:01:59 -0400

    Fix SDC tests

--- tests/z80/test_sdc.fs
+++ tests/z80/test_sdc.fs
@@ -1,8 +1,9 @@
 212 LOAD ( z80a )
-: SDC_SPI 4 ;
-: SDC_CSLOW 5 ;
-: SDC_CSHIGH 6 ;
-602 616 LOADR ( sdc )
+: SPI_DATA 4 ;
+: SPI_CSLOW 5 ;
+: SPI_CSHIGH 6 ;
+596 LOAD ( spi )
+603 616 LOADR ( sdc )
 
 0x0000 0x00 _crc16 0x0000 #eq
 0x0000 0x01 _crc16 0x1021 #eq
