6b895d07456e5776c1b3506e1ba46b9595cb5ffb
Virgil Dupras <hsoft@hardcoded.net>
2020-10-10 13:31:54 -0400

    sms: explicitly set ourselves in IM 1
    I'm not sure if not setting IM 1 explicitly can be the source of bugs,
    but SMS Power's tutorial mentions that everyone is doing it, so...

--- recipes/sms/xcomp.fs
+++ recipes/sms/xcomp.fs
@@ -19,7 +19,7 @@ SYSVARS 0x72 + CONSTANT PAD_MEM
 282 LOAD  ( boot.z80.decl )
 270 LOAD  ( xcomp overrides )
 
-0x100 JP, 0x63 ZFILL, ( 0x66 )
+DI, 0x100 JP, 0x62 ZFILL, ( 0x66 )
 RETN, 0x98 ZFILL, ( 0x100 )
 ( All set, carry on! )
 CURRENT @ XCURRENT !
@@ -33,6 +33,6 @@ CREATE ~FNT CPFNT7x7
 (entry) _
 ( Update LATEST )
 PC ORG @ 8 + !
-," VDP$ PAD$ " EOT,
+," VDP$ PAD$ (im1) " EOT,
 ORG @ 0x100 - 256 /MOD 2 PC! 2 PC!
 H@ 256 /MOD 2 PC! 2 PC!
