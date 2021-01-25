e3897c8585d96564be2a13c9c23af3586a0782a2
Virgil Dupras <hsoft@hardcoded.net>
2021-01-25 00:00:00 -0500

    - Make Z80 and 8086 assemblers more compact - Add 6809 assembler - Create a new "asm" folder in doc, making it clearer. - Add KEY> core word - Fix PS leak in DUMP - Improve VE

--- arch/8086/pcat/mbr.fs
+++ arch/8086/pcat/mbr.fs
@@ -1,4 +1,4 @@
-30 LOAD
+20 LOAD
 602 LOAD
 ORG @ |M 2 PC! 2 PC!
 H@ |M 2 PC! 2 PC!
--- emul/8086/xcomp.fs
+++ emul/8086/xcomp.fs
@@ -1,7 +1,7 @@
 0xff00 CONSTANT RS_ADDR
 0xfffa CONSTANT PS_ADDR
 RS_ADDR 0xa0 - CONSTANT SYSVARS
-30 LOAD   ( 8086 asm )
+20 LOAD   ( 8086 asm )
 262 LOAD  ( xcomp ) 270 LOAD  ( xcomp overrides )
 445 461 LOADR ( 8086 boot code )
 353 LOAD  ( xcomp core low )
